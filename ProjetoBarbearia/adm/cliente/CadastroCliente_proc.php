3bb05b52f3c1448cab685e9d908feba3e40cb95e
arcvsilv <56512605+arcvsilv@users.noreply.github.com>
2020-06-05 17:22:50 -0300

    Update CadastroCliente_proc.php

--- ProjetoBarbearia/adm/cliente/CadastroCliente_proc.php
+++ ProjetoBarbearia/adm/cliente/CadastroCliente_proc.php
@@ -10,12 +10,8 @@ $ddd = filter_input(INPUT_POST,'DDDTel',FILTER_DEFAULT);
 $telefone = filter_input(INPUT_POST,'TelFone',FILTER_DEFAULT);
 $Ativo = filter_input(INPUT_POST,'AtivoCliente',FILTER_DEFAULT);
 /*esses campos vão mudar, lembre-se*/
-var_dump( $Nome ,$DataNasc , $CPF,$RG, $ddd,$telefone,$Ativo );  
 $sql = "INSERT INTO CLIENTES (NOME,DATANASC,CPF,RG,DDD,TELEFONE,ATIVO) VALUES (?,?,?,?,?,?,? ) ";        
-var_dump($sql);
 $rs = mysqli_prepare($banco,$sql);
-var_dump($sql);
-mysqli_stmt_bind_param($rs,'sssssss', $Nome ,$DataNasc , $CPF,$RG, $ddd,$telefone,$Ativo) ;    
-var_dump($rs);                                        
+mysqli_stmt_bind_param($rs,'sssssss', $Nome ,$DataNasc , $CPF,$RG, $ddd,$telefone,$Ativo) ;                        
 mysqli_stmt_execute($rs);
 
