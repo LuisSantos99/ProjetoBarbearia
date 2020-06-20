554852c662f5354d7f2eca4c76cd01929a2311e1
LuisSantos99 <64987792+LuisSantos99@users.noreply.github.com>
2020-06-20 16:10:56 -0300

    Add files via upload

---
+++ ProjetoBarbearia/adm/cliente/excluir.php
@@ -0,0 +1,14 @@
+<?php
+require_once '../bd.php';
+
+$id = intval(filter_input(INPUT_GET,'id',FILTER_DEFAULT));
+
+$sql = "DELETE FROM clientes WHERE IDCLIENTE = ?";
+
+$rs = mysqli_prepare($banco,$sql); 
+
+mysqli_stmt_bind_param($rs,'i',$id);
+
+mysqli_stmt_execute($rs);
+
+header('location:index.php');
