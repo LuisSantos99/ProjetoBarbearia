9af2a69bba522505900f5c39d695ef2ecae78d7d
LuisSantos99 <64987792+LuisSantos99@users.noreply.github.com>
2020-06-20 16:13:53 -0300

    Delete inserir_barbeiro_proc.php

--- ProjetoBarbearia/adm/barbeiro/inserir_barbeiro_proc.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once ("../adm/bd.php");
-$nome= filter_input(INPUT_POST,'NOME',FILTER_DEFAULT);
-$cpf = filter_input(INPUT_POST,'cpf',FILTER_DEFAULT);
-$sql = "INSERT INTO  barbeiro (NOME,CPF) VALUES (?,?)";
-$rs = mysqli_prepare($banco,$sql);
-mysqli_stmt_bind_param($rs,'ss',$nome,$cpf);
-mysqli_stmt_execute($rs);
-header('location: listar.php');
