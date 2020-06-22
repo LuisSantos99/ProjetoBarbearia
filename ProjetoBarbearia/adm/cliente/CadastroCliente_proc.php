9992719655a3604940e47b995ed1b9ebb0f447b1
arcvsilv <56512605+arcvsilv@users.noreply.github.com>
2020-06-22 20:19:52 -0300

    Add files via upload

--- ProjetoBarbearia/adm/cliente/CadastroCliente_proc.php
+++ ProjetoBarbearia/adm/cliente/CadastroCliente_proc.php
@@ -6,12 +6,12 @@ $Nome = filter_input(INPUT_POST,'nomeCliente',FILTER_DEFAULT);
 $DataNasc = filter_input(INPUT_POST,'NascCliente',FILTER_DEFAULT);
 $CPF = filter_input(INPUT_POST,'CPFCliente',FILTER_DEFAULT);
 $RG = filter_input(INPUT_POST,'RGCliente',FILTER_DEFAULT);
-$ddd = filter_input(INPUT_POST,'DDDTel',FILTER_DEFAULT);
 $telefone = filter_input(INPUT_POST,'TelFone',FILTER_DEFAULT);
 $Ativo = filter_input(INPUT_POST,'AtivoCliente',FILTER_DEFAULT);
 /*esses campos vão mudar, lembre-se*/
-$sql = "INSERT INTO CLIENTES (NOME,DATANASC,CPF,RG,DDD,TELEFONE,ATIVO) VALUES (?,?,?,?,?,?,? ) ";        
+$sql = "INSERT INTO CLIENTES (NOME,DATANASC,CPF,RG,TELEFONE,ATIVO) VALUES (?,?,?,?,?,? ) ";        
 $rs = mysqli_prepare($banco,$sql);
-mysqli_stmt_bind_param($rs,'sssssss', $Nome ,$DataNasc , $CPF,$RG, $ddd,$telefone,$Ativo) ;                        
+mysqli_stmt_bind_param($rs,'ssssss', $Nome ,$DataNasc , $CPF,$RG,$telefone,$Ativo) ;                        
 mysqli_stmt_execute($rs);
 
+header('location: index.php');
--- ProjetoBarbearia/adm/cliente/index.php
+++ ProjetoBarbearia/adm/cliente/index.php
@@ -10,20 +10,26 @@ if ($radio == NULL)
 if ($pesquisa == NULL) {
 
   if ($radio == 'ativo')
-    $sql = "SELECT * FROM CLIENTES WHERE ATIVO = 'on' ORDER BY NOME";
+    $sql = "SELECT IDCLIENTE,NOME,DATE_FORMAT(DATANASC,'%d/%m/%Y') AS DATANASC,RG,CPF,TELEFONE 
+            FROM CLIENTES WHERE ATIVO = 'on' ORDER BY NOME";
   else  if ($radio == 'inativo')
-    $sql = "SELECT * FROM CLIENTES WHERE ATIVO = '0' ORDER BY NOME";
+    $sql = "SELECT IDCLIENTE,NOME,DATE_FORMAT(DATANASC,'%d/%m/%Y') AS DATANASC,RG,CPF,TELEFONE  
+            FROM CLIENTES WHERE ATIVO = '0' ORDER BY NOME";
 
   else
-    $sql = "SELECT * FROM CLIENTES ORDER BY NOME";
+    $sql = "SELECT IDCLIENTE,NOME,DATE_FORMAT(DATANASC,'%d/%m/%Y') AS DATANASC,RG,CPF,TELEFONE 
+    FROM CLIENTES ORDER BY NOME";
 } else {
 
   if ($radio == 'ativo')
-    $sql = "SELECT * FROM CLIENTES WHERE NOME LIKE '%" . $pesquisa . "%' AND ATIVO = 'on' ORDER BY NOME";
+    $sql = "SELECT IDCLIENTE,NOME,DATE_FORMAT(DATANASC,'%d/%m/%Y') AS DATANASC,RG,CPF,TELEFONE 
+            FROM CLIENTES WHERE NOME LIKE '%" . $pesquisa . "%' AND ATIVO = 'on' ORDER BY NOME";
   else if ($radio == 'inativo')
-    $sql = "SELECT * FROM CLIENTES WHERE NOME LIKE '%" . $pesquisa . "%' AND ATIVO = '0' ORDER BY NOME";
+    $sql = "SELECT IDCLIENTE,NOME,DATE_FORMAT(DATANASC,'%d/%m/%Y') AS DATANASC,RG,CPF,TELEFONE  
+            FROM CLIENTES WHERE NOME LIKE '%" . $pesquisa . "%' AND ATIVO = '0' ORDER BY NOME";
   else
-    $sql = "SELECT * FROM CLIENTES WHERE NOME LIKE '%" . $pesquisa . "%'  ORDER BY NOME";
+    $sql = "SELECT IDCLIENTE,NOME,DATE_FORMAT(DATANASC,'%d/%m/%Y') AS DATANASC,RG,CPF,TELEFONE 
+            FROM CLIENTES WHERE NOME LIKE '%" . $pesquisa . "%'  ORDER BY NOME";
 }
 
 $resultado = mysqli_query($banco, $sql);
@@ -37,71 +43,109 @@ $_POST['radio'] = $radio;
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Listar Clientes</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-  <link rel="stylesheet" href="../../css/adm.css">
+  <link rel="stylesheet" href="../../css/global.css">
+  <link rel="stylesheet" href="../../css/paginaInicial.css">
+
+  <script src="https://kit.fontawesome.com/3b5310efad.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
+  <header class="topo">
+    <img src="../../img/logo.png" alt="Logo do site">
+    <h1>Clientes<h1>
+        <a class="botao" href="logout.php">Sair (X)</a>
+  </header>
 
-  <h1>Listar Clientes</h1>
-  <hr>
-  <form name="frmBusca" method="post" action="<?php echo $_SERVER['PHP_SELF'] ?>?a=buscar">
-    <div class="form-row">
-      <div class="form-group col-md-">
-        <label for="cor1">Filtrar por:</label>
+  <div class="container">
+    <div class="form-row" style="margin-top:10px;">
+      <div class="form-group col-md-11">
+        <a href="../../paginaInicial.html">
+          <label> Página Inicial </label>
+        </a>
+        <label>|</label>
+        <label>Clientes</label>
       </div>
-      <div class="form-group col-md-">
-        <input id="ativo" type="radio" name="radio" value="ativo" <?php echo ($radio == "ativo") ? "checked" : null; ?> />
-        <label for="cor1">Ativos</label>
-      </div>
-      <div class="form-group col-md-">
-        <input id="inativo" type="radio" name="radio" value="inativo" value="ativo" <?php echo ($radio == "inativo") ? "checked" : null; ?> />
-        <label for="cor2">Inativos</label>
-      </div>
-      <div class="form-group col-md-">
-        <input id="ambos" type="radio" name="radio" value="ambos" value="ativo" <?php echo ($radio == "ambos") ? "checked" : null; ?> />
-        <label for="cor3">Ambos</label>
-      </div>
-      <div class="form-group col-md-7">
-        <input class="form-control mr-sm-2" type="search" name="pesquisa" id="pesquisa" placeholder="Pesquisar" aria-label="Pesquisar">
-
-      </div>
-      <div class="form-group col-md-2">
-        <button class="btn btn-info" id='btPesquisar' type="submit">Pesquisar</button>
-        <img scr="../../img/pesquisar.png">
+      <div class="form-group col-md">
+        <a href="cadastro_cliente.php">
+          <button class="btn btn-danger" id='btInserir' style="width:80px; height:30px;">
+            <i class="fas fa-plus-circle service-icon" id="btInserir" style="font-size:2vw; margin-top :-6px;"></i>
+          </button>
+        </a>
       </div>
     </div>
-  </form>
 
-  <table class="table table-borderless table-dark">
-    <thead>
-      <tr>
-        <th scope="col">Nome</th>
-        <th scope="col">Data de Nascimento</th>
-        <th scope="col">CPF</th>
-        <th scope="col">RG</th>
-        <th scope="col">Telefone</th>
-      </tr>
-    </thead>
-    <tbody>
-      <?php
-      while ($clientes = mysqli_fetch_assoc($resultado)) :
-      ?>
+    <form name="frmBusca" method="post" action="<?php echo $_SERVER['PHP_SELF'] ?>?a=buscar">
+      <div class="form-row" style="margin-top:10px; ">
+        <br>
+        <div class="form-group col-md-" style="padding-top: 5px;">
+          <label for="cor1">Filtrar por:</label>
+        </div>
+        <div class="form-group col-md-" style="padding-top: 5px;">
+          <input id="ativo" type="radio" name="radio" value="ativo" <?php echo ($radio == "ativo") ? "checked" : null; ?> />
+          <label for="cor1">Ativos</label>
+        </div>
+        <div class="form-group col-md-" style="padding-top: 5px;">
+          <input id="inativo" type="radio" name="radio" value="inativo" value="ativo" <?php echo ($radio == "inativo") ? "checked" : null; ?> />
+          <label for="cor2">Inativos</label>
+        </div>
+        <div class="form-group col-md-" style="padding-top: 5px;">
+          <input id="ambos" type="radio" name="radio" value="ambos" value="ativo" <?php echo ($radio == "ambos") ? "checked" : null; ?> />
+          <label for="cor3">Ambos</label>
+        </div>
+        <div class="form-group col-md-" style="margin-left: 15px;">
 
+        </div>
+        <div class="form-group col-md-7">
+          <input class="form-control mr-sm-" type="search" name="pesquisa" id="pesquisa" placeholder="Pesquisar" aria-label="Pesquisar">
+        </div>
+        <div class="form-group col-md-" id="btPesquisar">
+          <button class="btn btn-info" id='btPesquisar' type="submit" style="width:120px; height:40px;">
+            <i class="fas fa-search service-icon" id="btPesquisar" style="font-size:2vw;"></i>
+          </button>
+        </div>
+      </div>
+      <hr>
+    </form>
+
+    <table class="table table-bordered" style="background-color: white;">
+      <thead class="thead-dark">
         <tr>
-          <td><?= $clientes['NOME'] ?></td>
-          <td><?= $clientes['DATANASC'] ?> </td>
-          <td><?= $clientes['CPF'] ?></td>
-          <td><?= $clientes['RG'] ?></td>
-          <td><?= $clientes['TELEFONE'] ?></td>
-          <td>
-            <!-- COLOCAR ICONES -->
-            <a class="btn btn-success" href="editar.php?id=<?= $clientes['IDCLIENTE'] ?>" role="button"> Editar </a>
-            <a class="btn btn-danger" href="excluir.php?id=<?= $clientes['IDCLIENTE'] ?>" role="button"> Excluir</a>
-          </td>
+          <th scope="col">Nome</th>
+          <th scope="col">Data de Nascimento</th>
+          <th scope="col">CPF</th>
+          <th scope="col">RG</th>
+          <th scope="col">Telefone</th>
+          <th scope="col">Ações</th>
         </tr>
-      <?php endwhile; ?>
-    </tbody>
-  </table>
+      </thead>
+      <tbody>
+        <?php
+        while ($clientes = mysqli_fetch_assoc($resultado)) :
+        ?>
+
+          <tr>
+            <td><?= $clientes['NOME'] ?></td>
+            <td><?= $clientes['DATANASC'] ?> </td>
+            <td><?= $clientes['CPF'] ?></td>
+            <td><?= $clientes['RG'] ?></td>
+            <td><?= $clientes['TELEFONE'] ?></td>
+            <td>
+              <!-- COLOCAR ICONES -->
+              <center>
+                <a class="btn btn-success" href="editar.php?id=<?= $clientes['IDCLIENTE'] ?>" role="button" style="height:30px;">
+                  <i class="fas fa-edit service-icon" id="btEditar" style="font-size:0.7vw; "></i>
+                </a>
+
+                <a class="btn btn-danger" href="excluir.php?id=<?= $clientes['IDCLIENTE'] ?>" role="button" style="height:30px;">
+                  <i class="fas fa-trash service-icon" id="btExcluir" style="font-size:0.7vw; "></i>
+                </a>
+              </center>
+            </td>
+          </tr>
+        <?php endwhile; ?>
+      </tbody>
+    </table>
+  </div>
 
 </body>
 
